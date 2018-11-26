de3c71ae72a43c03599412b5ff2440f56e014abd
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-26 13:53:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Code your solution in this file.
 
 function lowerCaseDrivers(drivers) {
-
+  return drivers.map(function(driver){
+    return driver.toLowerCase();
+  });
 }
 
 function attributesToPhrase() {
